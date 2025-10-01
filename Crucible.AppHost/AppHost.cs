e6fb16d0067f25d6152af1d9eaed6fe03e725c19
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2025-10-01 15:48:50 -0400

    wip

--- Crucible.AppHost/AppHost.cs
+++ Crucible.AppHost/AppHost.cs
@@ -1,11 +1,28 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var pgPass = builder.AddParameter("pgPass");
-
-var postgres = builder.AddPostgres("postgres", password: pgPass)
+var postgres = builder.AddPostgres("postgres")
     .WithDataVolume()
+    .WithLifetime(ContainerLifetime.Persistent)
     .WithPgAdmin();
 
-var playerDb = postgres.AddDatabase("player");
+//var postgresPasswordExpression = postgres.Resource.PasswordParameter;
+
+var playerDb = postgres.AddDatabase("playerDb", "player");
+
+// var keycloakDb = postgres.AddDatabase("keycloakDb", "keycloak");
+
+var keycloak = builder.AddKeycloak("keycloak", 8080)
+    .WithRealmImport("./resources/crucible-realm.json");
+
+var playerApi = builder.AddProject<Projects.Player_Api>("player-api", launchProfileName: "Player.Api")
+    .WaitFor(postgres)
+    .WaitFor(keycloak)
+    .WithHttpHealthCheck("api/health/ready")
+    .WithReference(playerDb, "PostgreSQL")
+    .WithEnvironment("Database__Provider", "PostgreSQL")
+    .WithEnvironment("Authorization__Authority", "http://localhost:8080/realms/crucible")
+    .WithEnvironment("Authorization__AuthorizationUrl", "http://localhost:8080/realms/crucible/protocol/openid-connect/auth")
+    .WithEnvironment("Authorization__TokenUrl", "http://localhost:8080/realms/crucible/protocol/openid-connect/token")
+    .WithEnvironment("Authorization_ClientId", "player.api");
 
 builder.Build().Run();
