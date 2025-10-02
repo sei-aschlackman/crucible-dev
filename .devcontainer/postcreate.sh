9cbe29c03e6b5d6f39fab26df500ddf585bacb14
Andrew Schlackman <72105194+sei-aschlackman@users.noreply.github.com>
2025-10-02 16:23:30 -0400

    wip - change volume permissions before clone

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -3,11 +3,12 @@
 # Show git dirty status in zsh prompt
 git config devcontainers-theme.show-dirty 1
 
+sudo chown -R $(whoami): /home/vscode/.microsoft
+sudo chown -R $(whoami): /mnt/data/
+
 chmod +x .devcontainer/clone-repos.sh
 .devcontainer/clone-repos.sh
 
 dotnet tool install -g Aspire.Cli
 dotnet dev-certs https --trust
-sudo chown -R $(whoami): /home/vscode/.microsoft
-sudo chown -R $(whoami): /mnt/data/
 dotnet restore
