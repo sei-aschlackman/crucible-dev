3c9180b40b0d2fa95baa9a75ec8647d73da462eb
Ben Stein <115497763+sei-bstein@users.noreply.github.com>
2025-10-07 15:04:37 -0400

    Update repos.json and clone script (#8)

--- .devcontainer/clone-repos.sh
+++ .devcontainer/clone-repos.sh
@@ -3,16 +3,19 @@ set -euo pipefail
 
 MANIFEST=.devcontainer/repos.json
 
-jq -c '.repositories[]' $MANIFEST | while read repo; do
-    NAME=$(echo $repo | jq -r .name)
-    GROUP=$(echo $repo | jq -r .group)
-    URL=$(echo $repo | jq -r .url)    
-    TARGET="/mnt/data/crucible/$GROUP/$NAME"
+jq -c '.groups[]' $MANIFEST | while read group; do
+    GROUP=$(echo $group | jq -r .name)
 
-    if [ ! -d "$TARGET" ]; then
-        echo "Cloning $NAME..."
-        git clone "$URL" "$TARGET"
-    else
-        echo "$NAME already exists, skipping."
-    fi
+    echo "$group" | jq -c '.repos[]' | while read -r repo; do
+        NAME=$(echo $repo | jq -r .name)
+        URL=$(echo $repo | jq -r .url) 
+        TARGET="/mnt/data/crucible/$GROUP/$NAME"
+        
+        if [ ! -d "$TARGET" ]; then
+            echo "Cloning $NAME..."
+            git clone "$URL" "$TARGET"
+        else
+            echo "$NAME already exists, skipping."
+        fi
+    done
 done
